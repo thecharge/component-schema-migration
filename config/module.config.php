23206dc0258dd345753b7d692d3a103479d7a867
Dima Denisenko <dima@wellcart.org>
2016-09-02 14:22:36 +0300

    Use ::class constant notation for configuring services.

--- config/module.config.php
+++ config/module.config.php
@@ -6,6 +6,8 @@
  * @license    http://www.opensource.org/licenses/BSD-3-Clause New BSD License
  */
 
+namespace WellCart\SchemaMigration;
+
 return [
     'controllers'      => [
         'invokables' => [
