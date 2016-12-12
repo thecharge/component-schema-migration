f545af61bc3b62d28d371550f03da4b257e7fa2c
Dima Deisenko <dmitry.denisenko@amadeus.com>
2016-12-12 12:25:32 +0200

    Set declare(strict_types = 1); to all classes.

--- tests/support/Traits/UnitTesterActions.php
+++ tests/support/Traits/UnitTesterActions.php
@@ -5,6 +5,8 @@
  * @copyright  Copyright (c) 2016 WellCart Development Team    http://wellcart.org/
  * @license    http://www.opensource.org/licenses/BSD-3-Clause New BSD License
  */
+declare(strict_types = 1);
+
 namespace WellCart\SchemaMigration\Test\Traits;
 
 trait UnitTesterActions
